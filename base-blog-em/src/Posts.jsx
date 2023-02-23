31ad30f69a59c3e4a7362f258ca2147404fd7c14
raccoon-ccoder <qorwjddus96@naver.com>
2023-02-23 22:19:04 +0900

    기본적인  useQuery 사용방법

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -9,14 +10,24 @@ async function fetchPosts() {
   );
   return response.json();
 }
-
+// isFetching > isLoading
+// isFetching - async query function hasn't yet resolved
+// isLoading - no cached data, plus isFetcing
+// 나중에 Pagination에서는 캐시된 데이터 존재 유무를 판단해야 하기에 isFetcing, isLoading의 차이점을 알아야 함! (캐시된 데이터 유무)
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
-
+  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts);
+  if (isLoading) return <h3>Loading ...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>someting went wrong...</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
   return (
     <>
       <ul>
