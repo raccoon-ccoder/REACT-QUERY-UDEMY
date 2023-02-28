a991d905bff671d06c30354399bc493bac131440
jybaek96 <qorwjddus96@naver.com>
2023-02-28 23:07:13 +0900

    useInfiniteQuery 기초

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -8,6 +9,28 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined, // lastPage.next가 거짓이면 undefined 반환
+      // lastPage는 마지막에 실행된 쿼리함수 결과값으로 star wars api data 결과값에는 다음 페이지로 이동할 수 있는 url이 담긴 next property가 있기에 해당 프로퍼티를 전달
+    }
+  );
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map((pageData) => {
+        return pageData.results.map((person) => {
+          return (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            />
+          );
+        });
+      })}
+    </InfiniteScroll>
+  );
 }
